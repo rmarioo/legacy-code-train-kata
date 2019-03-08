44fff48808776f4b80b72bfe1f2de697279b7e5f
Mario Russo <mario.russo@lastminute.com>
2019-03-08 22:25:46 +0100

    creating coaches abstraction

--- src/main/java/com/rmarioo/sample/trainlegacy/Train.java
+++ src/main/java/com/rmarioo/sample/trainlegacy/Train.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Train {
-  public List<Coach> coaches = new ArrayList<>();
+  public List<Coach> coaches;
     public String name;
 
     public Train(String name,List<Seat> seats) {
