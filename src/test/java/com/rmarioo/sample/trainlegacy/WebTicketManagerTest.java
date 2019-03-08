dc50da0070e54226955fa89bd0404866fcb8cef9
Mario Russo <mario.russo@lastminute.com>
2019-03-08 08:58:08 +0100

    break dependencies + better test name

--- src/test/java/com/rmarioo/sample/trainlegacy/WebTicketManagerTest.java
+++ src/test/java/com/rmarioo/sample/trainlegacy/WebTicketManagerTest.java
@@ -7,17 +7,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class WebTicketManagerTest
 {
 
   @Test
-  public void aaa() throws IOException {
+  public void when_requested_seats_exceeds_70_availability_return_empty_registration() throws IOException {
 
-    WebTicketManager webTicketManager = new WebTicketManager();
+    WebTicketManager webTicketManager = new TestableWebTicketManager();
     Reservation reservation = webTicketManager.reserve("first", 6);
 
-    Assert.assertThat(reservation,is(new Reservation()));
+    Assert.assertThat(reservation,is(new Reservation("first","",Arrays.asList())));
   }
 
+  private class TestableWebTicketManager extends WebTicketManager {
+    @Override
+    protected Train findTrain(String trainId) {
+      return new Train("first", Arrays.asList(
+          new Seat("A",1),
+          new Seat("A",2),
+          new Seat("A",3),
+          new Seat("A",4),
+          new Seat("B",1),
+          new Seat("B",2),
+          new Seat("B",3),
+          new Seat("B",4)
+      ));
+    }
+  }
 }
