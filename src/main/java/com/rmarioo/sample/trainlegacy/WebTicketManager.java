0faa7497058ca2dd462f4a93f45144b9609189e1
Mario Russo <mario.russo@lastminute.com>
2019-03-10 11:42:32 +0100

    extracting collaborators

--- src/main/java/com/rmarioo/sample/trainlegacy/WebTicketManager.java
+++ src/main/java/com/rmarioo/sample/trainlegacy/WebTicketManager.java
@@ -9,55 +9,35 @@ public class WebTicketManager
   private final BookingService bookingService;
   private  TrainDataRepository trainDataRepository;
 
-  public WebTicketManager(TrainDataRepository trainDataRepository, BookingService bookingService) {
+  public WebTicketManager(TrainDataRepository trainDataRepository,
+                          BookingService bookingService)
+  {
     this.trainDataRepository = trainDataRepository;
     this.bookingService = bookingService;
   }
 
   public Reservation reserve(String trainId, int requestedSeats) {
 
-    Train train = findTrain(trainId);
+    Train train = trainDataRepository.findTrain(trainId);
 
-    boolean enoughSeats = train.coaches.stream().anyMatch(coach -> coach.hasEnoughSeatsFor(requestedSeats));
     if (train.requestDoesnotExceeds70perc(requestedSeats))
     {
       List<Seat> availableSeats = train.findAvailableSeats(requestedSeats);
 
-      return (availableSeats.size() == requestedSeats)
-        ? reserveSeats(trainId, availableSeats)
-        : noSeatsReservation(trainId);
+      String bookingRef    = bookingService.createbookingRef();
+      Boolean isSuccessful = bookingService.reserve(trainId, availableSeats, bookingRef);
+
+      return (isSuccessful && availableSeats.size() == requestedSeats)
+          ? new Reservation(trainId, bookingRef, availableSeats)
+          : noSeatsReservation(trainId);
     }
 
     return noSeatsReservation(trainId);
   }
 
-  protected Reservation noSeatsReservation(String trainId) {
+  private Reservation noSeatsReservation(String trainId) {
     return new Reservation(trainId, "", Arrays.asList());
   }
-
-  protected Reservation reserveSeats(String trainId, List<Seat> availableSeats) {
-
-
-    String bookingRef = createBookingReference();
-
-    Boolean isSuccessful = makeReservation(trainId, availableSeats, bookingRef);
-
-    return  (isSuccessful) ? new Reservation(trainId, bookingRef, availableSeats)
-                           : noSeatsReservation(trainId);
-  }
-
-  protected Boolean makeReservation(String trainId, List<Seat> availableSeats, String bookingRef) {
-    return bookingService.reserve(trainId, availableSeats, bookingRef);
-    }
-
-
-  protected String createBookingReference() {
-      return bookingService.createbookingRef();
-    }
-
-  protected Train findTrain(String trainId) {
-        return trainDataRepository.findTrain(trainId);
-    }
 
 
 }
