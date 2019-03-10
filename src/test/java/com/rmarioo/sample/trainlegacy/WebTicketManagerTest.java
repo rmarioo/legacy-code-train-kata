be01fc2118e78f513c2c29a2337b147d8ad8703c
Mario Russo <mario.russo@lastminute.com>
2019-03-10 11:38:36 +0100

    extracting collaborators

--- src/test/java/com/rmarioo/sample/trainlegacy/WebTicketManagerTest.java
+++ src/test/java/com/rmarioo/sample/trainlegacy/WebTicketManagerTest.java
@@ -25,7 +25,8 @@ public class WebTicketManagerTest
   @Test
   public void when_requested_seats_below_70_availability_return_a_reservation() throws IOException {
 
-    WebTicketManager webTicketManager = new TestableWebTicketManager();
+    WebTicketManager webTicketManager = new WebTicketManager(new StubTrainDataRepository(),
+        new StubBookingReserveService(true));
     Reservation reservation = webTicketManager.reserve("first", 2);
 
     Assert.assertThat(reservation,is(new Reservation("first","ref1",
@@ -37,7 +38,8 @@ public class WebTicketManagerTest
   @Test
   public void when_reserve_fails_return_empty_reservation() throws IOException {
 
-    WebTicketManager webTicketManager = new TestableWebTicketManager(false);
+    WebTicketManager webTicketManager = new WebTicketManager(new StubTrainDataRepository(),
+        new StubBookingReserveService(false));
     Reservation reservation = webTicketManager.reserve("first", 2);
 
     Assert.assertThat(reservation,is(new Reservation("first","",Arrays.asList())));
@@ -47,7 +49,8 @@ public class WebTicketManagerTest
   @Test
   public void sameReservasion_goes_across_different_coaches() throws IOException {
 
-    WebTicketManager webTicketManager = new TestableWebTicketManager();
+    WebTicketManager webTicketManager = new WebTicketManager(new StubTrainDataRepository(),
+        new StubBookingReserveService(true));
     Reservation reservation = webTicketManager.reserve("first", 5);
 
     Assert.assertThat(reservation,is(new Reservation("first","ref1",
@@ -60,47 +63,6 @@ public class WebTicketManagerTest
         ))));
   }
 
-
-  private class TestableWebTicketManager extends WebTicketManager
-  {
-    private final boolean reservationSuccessfull;
-
-    public TestableWebTicketManager(boolean reservationSuccessfull) {
-
-      super(new DefaultTrainDataRepository(), new DefaultBookingService());
-      this.reservationSuccessfull = reservationSuccessfull;
-    }
-
-    public TestableWebTicketManager() {
-      this(true);
-    }
-
-    @Override
-    protected String createBookingReference() {
-      return "ref1";
-    }
-
-    @Override
-    protected Boolean makeReservation(String trainId, List<Seat> availableSeats, String bookingRef) {
-      return reservationSuccessfull;
-    }
-
-    @Override
-    protected Train findTrain(String trainId) {
-
-
-      return new Train("first", Arrays.asList(
-          new Seat("A",1),
-          new Seat("A",2),
-          new Seat("A",3),
-          new Seat("A",4),
-          new Seat("B",1),
-          new Seat("B",2),
-          new Seat("B",3),
-          new Seat("B",4)
-      ));
-    }
-  }
 
   private class StubTrainDataRepository implements TrainDataRepository {
     @Override
